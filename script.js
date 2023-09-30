3d09354158d1af671dc0f95a231a997a63fed204
Itamar <146496290+ItamarAshwal@users.noreply.github.com>
2023-09-30 18:19:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Your existing head content -->
+
+    <!-- Link to the JavaScript file -->
+    <script src="script.js" defer></script>
+
+    <!-- Your existing style link -->
+</head>
+<body>
+    <!-- Your existing body content -->
+
+    <!-- Add a button or any element to trigger dark mode -->
+    <button onclick="toggleDarkMode()">Toggle Dark Mode</button>
+</body>
+</html>
